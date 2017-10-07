ddb42f3bd11878163eab67a0a042a1ab6f05fed8
Boud <maxime.boudreault.0@gmail.com>
2017-10-06 20:56:53 -0400

    Add accessors to SuperPower class

--- Assets/SuperPower.cs
+++ Assets/SuperPower.cs
@@ -6,4 +6,13 @@ public class SuperPower : MonoBehaviour
 {
     [SerializeField] string power;
     [SerializeField] Texture2D icon;
+
+    public string Power { get { return power; } }
+
+    public Texture2D Icon { get { return icon; } }
+
+    void Start()
+    {
+        Debug.LogError("SuperPowers should stay prefabs and never be instantiated.");
+    }
 }
