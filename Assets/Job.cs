811c575d99a9716253736cd6eac278fbb5c63eb7
Boud <maxime.boudreault.0@gmail.com>
2017-10-06 20:45:16 -0400

    Add some base classes

---
+++ Assets/Job.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Job : MonoBehaviour
+{
+    [SerializeField] string name;
+    [SerializeField] Texture2D icon;
+    [SerializeField] SuperPower[] acceptedPowers;
+    [SerializeField] SuperPower[] forbiddenPowers;
+}
---
+++ Assets/SuperPower.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperPower : MonoBehaviour
+{
+    [SerializeField] string power;
+    [SerializeField] Texture2D icon;
+}
