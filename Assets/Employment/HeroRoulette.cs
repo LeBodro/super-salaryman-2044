57a934a1d96cdbdfab8f744c6f5ec7b79b1fca20
Boud <maxime.boudreault.0@gmail.com>
2017-10-08 14:06:05 -0400

    Add roulette script

---
+++ Assets/Employment/HeroRoulette.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class HeroRoulette : MonoBehaviour
+{
+    int selectionCount;
+
+    void Start()
+    {
+        selectionCount = transform.childCount - 1;
+        int nextHeroIndex = Random.Range(0, transform.childCount);
+        SetHero(nextHeroIndex);
+    }
+
+    public void Turn()
+    {
+        int nextHeroIndex = Random.Range(0, selectionCount);
+        SetHero(nextHeroIndex);
+    }
+
+    void SetHero(int index)
+    {
+        transform.GetChild(selectionCount).gameObject.SetActive(false);
+        var nextHero = transform.GetChild(index);
+        nextHero.gameObject.SetActive(true);
+        nextHero.SetAsLastSibling();
+    }
+
+    public bool debug;
+
+    void Update()
+    {
+        if (debug)
+        {
+            debug = false;
+            Turn();
+        }
+    }
+}
