08a391a25dcd6dd300fbd2243968e34f8f3b34cb
Boud <maxime.boudreault.0@gmail.com>
2017-10-07 21:09:00 -0400

    Add effect

---
+++ Assets/Effects/Effect.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    public virtual void Play()
+    {
+    }
+}
---
+++ Assets/Effects/WrongEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WrongEffect : Effect
+{
+    [SerializeField] Image overlay;
+    [SerializeField] float fadeOutEndTime;
+    [SerializeField] Color overlayColor;
+
+    float fadeOutTime;
+
+    void Start()
+    {
+        fadeOutTime = fadeOutEndTime;
+    }
+
+    public override void Play()
+    {
+        CrackleAudio.SoundController.PlaySound("wrong");
+        overlay.color = overlayColor;
+        fadeOutTime = 0;
+    }
+
+    void Update()
+    {
+        if (fadeOutTime < fadeOutEndTime)
+        {
+            fadeOutTime += Time.deltaTime;
+            overlay.color = Color.Lerp(overlay.color, Color.clear, fadeOutTime / fadeOutEndTime);
+        }
+    }
+}
